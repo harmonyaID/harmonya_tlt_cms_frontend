ff2922d492b0ad6a0c734658e6caa0ae3fbc6332
arbimauday <arbimauday@gmail.com>
2026-07-26 01:06:32 +0800

    task: change sadhes layout

--- src/helper/convertText.helper.ts
+++ src/helper/convertText.helper.ts
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react'
+
 export const textSlug = (text: string) => {
     return text
         .toString()
@@ -8,4 +10,20 @@ export const textSlug = (text: string) => {
         .replace(/[^a-z0-9\s-]/g, '') // hapus karakter selain huruf/angka/spasi/strip
         .replace(/\s+/g, '-') // spasi jadi strip
         .replace(/-+/g, '-')
+}
+
+export const textToId = (text?: string, format = '') => {
+    const words = text.trim().toLowerCase().split(/\s+/)
+
+    if (format === 'camel') {
+        return words
+            .map((word, index) =>
+                index === 0
+                    ? word
+                    : word.charAt(0).toUpperCase() + word.slice(1),
+            )
+            .join('')
+    }
+
+    return words.join('-')
 }
