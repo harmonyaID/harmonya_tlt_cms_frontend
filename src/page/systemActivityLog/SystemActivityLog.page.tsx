12e660bd8864db0fd748a2af21c326b3e4e4d68d
ab-azmi <azmi.kiva@gmail.com>
2026-08-06 23:02:57 +0800

    custom activity log list ui

--- src/page/systemActivityLog/SystemActivityLog.page.tsx
+++ src/page/systemActivityLog/SystemActivityLog.page.tsx
@@ -1,30 +1,22 @@
-import { isEmpty } from 'lodash'
-import TabDataTable from '@/common/dataFeature/tabDataTable/TabDataTable.tsx'
+import { Fragment, useMemo } from 'react'
+import { capitalize, isEmpty, upperCase } from 'lodash'
 import Card from '@/component/card/Card.tsx'
-import CardNavTab from '@/component/card/CardNavTab.tsx'
+import { AvatarInTable } from '@/component/general/Avatar.tsx'
 import Pagination from '@/component/general/Pagination.tsx'
 import PreElement from '@/component/general/PreElement.tsx'
-import {
-    TblLineFirst,
-    TblLineSecond,
-    TblPointData,
-} from '@/component/general/TablePartial.tsx'
-import { PageTitle } from '@/component/general/TitleGeneral.tsx'
 import LoadingNotAvailable from '@/component/loading/LoadingNotAvailable.tsx'
-import LoadingSpinner from '@/component/loading/LoadingSpinner.tsx'
-import { objectTab, objectTabContent } from '@/config/objectNavTab.config.ts'
 import { configDefaultPagination } from '@/config/pagination.config.ts'
+import { LOG_ACTION_BADGE, LOG_ACTION_ICON } from '@/config/systemActivityLog.config.ts'
+import { actionFormatDateStrict } from '@/helper/actionFormatDate.helper.ts'
 import { isShowPagination } from '@/helper/base/condition.helper.ts'
+import joinClassNameHelper from '@/helper/base/joinClassName.helper.ts'
 import useDataListHook from '@/hook/base/useDataList.hook.ts'
-import { apiStaff } from '@/service/api/staff.api.ts'
 import { getLogActivity } from '@/service/api/systemManagement.api.ts'
+import { WrapFormContext } from '@/context/Form.context.tsx'
+import FormInput from '@/component/form/FormInput.tsx'
 
 const SystemActivityLogPage = () => {
     const ACTIVITY = 'Activity Log'
-
-    const SETTING_ACTION = 'Setting Action'
-
-    const SETTING_TYPE = 'Setting Type'
 
     const {
         __list,
@@ -46,6 +38,25 @@ const SystemActivityLogPage = () => {
         },
     })
 
+    const grouped = useMemo(() => {
+        return Object.values(
+            __list.reduce((acc, log) => {
+                const date = log.createdAt.split(' ')[0]
+
+                if (!acc[date]) {
+                    acc[date] = {
+                        date: upperCase(actionFormatDateStrict(date, 'dddd, D MMMM YYYY')),
+                        logs: [],
+                    }
+                }
+
+                acc[date].logs.push(log)
+
+                return acc
+            }, {}),
+        )
+    }, [__list])
+
     return (
         <>
             <Card title={ACTIVITY}>
@@ -53,56 +64,121 @@ const SystemActivityLogPage = () => {
                     <LoadingNotAvailable isLoading={__isLoading} />
                 ) : (
                     <>
-                        <div className="vstack gap-3 pb-4">
-                            {__list.map((vm, index) => {
-                                return (
-                                    <div
-                                        className="card card-body border-neutral-500"
-                                        key={index}>
-                                        <div className="row">
-                                            <div className="col-md-3">
-                                                <TblPointData title="Type">
-                                                    <span className="badge text-bg-tint-400 fw-medium fs-12 text-capitalize">
-                                                        {vm.type}
-                                                    </span>
-                                                </TblPointData>
-                                            </div>
-                                            <div className="col-md">
-                                                <TblPointData
-                                                    title="Description"
-                                                    isUseDefaultMargin={false}>
-                                                    <PreElement classNameFs="fs-12 fw-400">
-                                                        {vm.description}
-                                                    </PreElement>
-                                                </TblPointData>
-                                            </div>
+                        <WrapFormContext formRequest={{}}>
+                            <div className="row mb-2">
+                                <div className="col-md-3">
+                                    <FormInput
+                                        placeholder="Search"
+                                        name="search"
+                                    />
+                                </div>
+                            </div>
+                        </WrapFormContext>
 
-                                            <div className="col-md-1">
-                                                <TblPointData
-                                                    title="Action"
-                                                    classNameValue="text-capitalize"
-                                                    value={vm.action}
-                                                />
-                                            </div>
-                                            <div className="col-md-2">
-                                                <TblPointData
-                                                    title="Caused By Name"
-                                                    value={
-                                                        vm?.causedByName || '-'
-                                                    }
-                                                />
-                                            </div>
+                        {grouped.map((log: any, index) => (
+                            <Fragment key={index}>
+                                <p className="fw-medium text-neutral-300 fs-13">
+                                    {log.date}
+                                </p>
 
-                                            <div className="col-md-1">
-                                                <TblPointData title="Created At">
-                                                    {vm.createdAt}
-                                                </TblPointData>
+                                <div className="vstack gap-3 pb-2">
+                                    {log.logs.map((vm, idx) => {
+                                        const Icon = LOG_ACTION_ICON[vm.action]
+
+                                        return (
+                                            <div
+                                                className="card card-body border-neutral-500"
+                                                key={index + '_' + idx}>
+                                                <div className="row">
+                                                    <div className="col-auto">
+                                                        <span
+                                                            className={joinClassNameHelper(
+                                                                'size-35 rounded-2 d-flex justify-content-center align-items-center',
+                                                                LOG_ACTION_BADGE[
+                                                                    vm.action
+                                                                ],
+                                                            )}>
+                                                            <Icon
+                                                                variant="Bulk"
+                                                                size={20}
+                                                            />
+                                                        </span>
+                                                    </div>
+                                                    <div className="col-md">
+                                                        <div className="vstack gap-2">
+                                                            <div className="d-flex justify-content-between align-items-start">
+                                                                <span
+                                                                    className={joinClassNameHelper(
+                                                                        'w-fit-content badge rounded-1 fw-medium fs-12 text-capitalize',
+                                                                        LOG_ACTION_BADGE[
+                                                                            vm
+                                                                                .action
+                                                                        ],
+                                                                    )}>
+                                                                    {upperCase(
+                                                                        vm.action,
+                                                                    )}
+                                                                </span>
+
+                                                                <p className="m-0">
+                                                                    {
+                                                                        vm.createdAt
+                                                                    }
+                                                                </p>
+                                                            </div>
+                                                            <AvatarInTable
+                                                                isSmall
+                                                                title={
+                                                                    vm?.causedByName ||
+                                                                    '-'
+                                                                }
+                                                            />
+                                                            <PreElement
+                                                                classNameFs="fs-12 fw-400"
+                                                                className="bg-neutral-600">
+                                                                <span className="fw-bold text-capitalize">
+                                                                    {vm.type.replaceAll(
+                                                                        '_',
+                                                                        ' ',
+                                                                    )}
+                                                                    :
+                                                                </span>{' '}
+                                                                {vm.description}
+                                                            </PreElement>
+                                                        </div>
+                                                    </div>
+
+                                                    {/*<div className="col-md-1">*/}
+                                                    {/*    <TblPointData*/}
+                                                    {/*        title="Action"*/}
+                                                    {/*        classNameValue="text-capitalize"*/}
+                                                    {/*        value={vm.action}*/}
+                                                    {/*    />*/}
+                                                    {/*</div>*/}
+                                                    {/*<div className="col-md-2">*/}
+                                                    {/*    <TblPointData*/}
+                                                    {/*        title="Caused By Name"*/}
+                                                    {/*        value={*/}
+                                                    {/*            vm?.causedByName ||*/}
+                                                    {/*            '-'*/}
+                                                    {/*        }*/}
+                                                    {/*    />*/}
+                                                    {/*</div>*/}
+
+                                                    {/*<div className="col-md-1">*/}
+                                                    {/*    <TblPointData title="Created At">*/}
+                                                    {/*       */}
+                                                    {/*    </TblPointData>*/}
+                                                    {/*</div>*/}
+                                                </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                )
-                            })}
-                        </div>
+                                        )
+                                    })}
+                                </div>
+
+                                <hr className="border-neutral-400" />
+                            </Fragment>
+                        ))}
 
                         {isShowPagination(__isLoading, __list, __pagination) ? (
                             <Pagination
