14d43b7cad8d9a03bf78212f93b813132655e3f1
ab-azmi <azmi.kiva@gmail.com>
2026-08-09 14:42:13 +0800

    feat: create boat table component

--- src/page/boat/Boat.page.tsx
+++ src/page/boat/Boat.page.tsx
@@ -1,24 +1,9 @@
 import useBoatMain from '@/page/boat/hook/useBoatMain.hook.ts'
 import CardListData from '@/component/card/CardListData.tsx'
 import {
-    BtnBase,
-    BtnCircleEdit,
-    BtnCircleRemove,
-    BtnCircleRestore,
     BtnDanger,
     BtnPrimary,
 } from '@/component/general/Button.tsx'
-import {
-    TblLineFirst,
-    TblLineFirstPrimary,
-    TblLineSecond,
-    TblPointData,
-} from '@/component/general/TablePartial.tsx'
-import TextTrueOrFalse from '@/component/general/TextTrueOrFalse.tsx'
-import TableThemeLogic from '@/common/table/TableTheme.logic.tsx'
-import { isShowPagination } from '@/helper/base/condition.helper.ts'
-import Pagination from '@/component/general/Pagination.tsx'
-import { configDefaultPagination } from '@/config/pagination.config.ts'
 import useChooseData from '@/hook/useChooseData.hook.ts'
 import actionModal from '@/helper/base/actionModal.helper.ts'
 import { MDGeneralRemove } from '@/config/modal.config.ts'
@@ -26,8 +11,7 @@ import ConfirmRemoveListLogic from '@/common/misc/ConfirmRemoveList.logic.tsx'
 import CreatePortalLayout from '@/component/layout/CreatePortal.layout.tsx'
 import { apiBoat } from '@/service/api/boatManage.api.ts'
 import FilterBarBasic from '@/common/misc/FilterBarBasic.tsx'
-import { useState } from 'react'
-import { isEmpty } from 'lodash'
+import BoatTable from '@/page/boat/component/BoatTable.tsx'
 
 const BoatPage = () => {
     const {
@@ -86,103 +70,17 @@ const BoatPage = () => {
                         clear: __actionClear,
                     }}
                 />
-                <div className="row overflow-y position-relative">
-                    <div className="col-md-12 table-responsive-md">
-                        <TableThemeLogic
-                            isLoading={__isLoading}
-                            isNoWrap
-                            ths={[
-                                'Name',
-                                'Boat Type',
-                                'Total Photos',
-                                'Total Promo Photos',
-                                'Status Active',
-                                'Created At',
-                                '',
-                            ]}
-                            tds={__list}>
-                            {__list.map((vm, index) => {
-                                return (
-                                    <tr
-                                        key={index}
-                                        title="Preview Detail"
-                                        className="cursor-pointer" onClick={() => __handleToDetail(vm.id)}>
-                                        <td>
-                                            <TblLineFirstPrimary
-                                                value={vm?.name}
-                                            />
-                                        </td>
-                                        <td>
-                                            <TblLineFirst
-                                                value={
-                                                    vm?.boatComponentTypeName
-                                                }
-                                            />
-                                        </td>
-                                        <td>
-                                            <TblLineSecond>
-                                                {vm?.photos.length || '-'}
-                                            </TblLineSecond>
-                                        </td>
-                                        <td>
-                                            <TblLineSecond>
-                                                {vm?.promoPhotos.length || '-'}
-                                            </TblLineSecond>
-                                        </td>
-
-                                        <td>
-                                            <TextTrueOrFalse
-                                                value={vm.isActive}
-                                            />
-                                        </td>
-                                        <td>
-                                            <TblLineSecond>
-                                                {vm?.createdAt || '-'}
-                                            </TblLineSecond>
-                                        </td>
-                                        <td>
-                                            <div className="hstack gap-2 justify-content-end">
-                                                <BtnCircleRemove
-                                                    actions={{
-                                                        remove: (e) => {
-                                                            e.stopPropagation()
-                                                            _handleChooseRemove(
-                                                                vm,
-                                                            )
-                                                        },
-                                                    }}
-                                                />
-
-                                                <BtnCircleEdit
-                                                    title="Edit Data"
-                                                    actions={{
-                                                        edit: (e) => {
-                                                            e.stopPropagation()
-                                                            __handleToEdit(
-                                                                vm.id,
-                                                            )
-                                                        },
-                                                    }}
-                                                />
-                                            </div>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                        </TableThemeLogic>
-                    </div>
-                </div>
-
-                {isShowPagination(__isLoading, __list, __pagination) ? (
-                    <Pagination
-                        onMove={(step) => __actionPagination(step)}
-                        className="mt-2"
-                        pagination={configDefaultPagination(
-                            __pagination,
-                            'totalPage',
-                        )}
-                    />
-                ) : null}
+                <BoatTable
+                    __list={__list}
+                    __isLoading={__isLoading}
+                    __pagination={__pagination}
+                    actions={{
+                        __handleChooseRemove: _handleChooseRemove,
+                        __actionPagination: __actionPagination,
+                        __handleToDetail: __handleToDetail,
+                        __handleToEdit: __handleToEdit,
+                    }}
+                />
             </CardListData>
 
             <CreatePortalLayout>
--- src/page/boat/BoatTrash.page.tsx
+++ src/page/boat/BoatTrash.page.tsx
@@ -1,26 +1,14 @@
 import useBoatMain from '@/page/boat/hook/useBoatMain.hook.ts'
 import CardListData from '@/component/card/CardListData.tsx'
 import {
-    BtnCircleRestore,
-    BtnCircleX,
-    BtnInfo,
     BtnPrimary,
 } from '@/component/general/Button.tsx'
-import {
-    TblLineFirst,
-    TblLineFirstPrimary,
-    TblLineSecond,
-} from '@/component/general/TablePartial.tsx'
-import TextTrueOrFalse from '@/component/general/TextTrueOrFalse.tsx'
-import TableThemeLogic from '@/common/table/TableTheme.logic.tsx'
-import { isShowPagination } from '@/helper/base/condition.helper.ts'
-import Pagination from '@/component/general/Pagination.tsx'
-import { configDefaultPagination } from '@/config/pagination.config.ts'
 import CreatePortalLayout from '@/component/layout/CreatePortal.layout.tsx'
 import { apiBoat } from '@/service/api/boatManage.api.ts'
 import FilterBarBasic from '@/common/misc/FilterBarBasic.tsx'
 import useTrash from '@/common/dataFeature/trash/hook/useTrash.ts'
 import TrashConfirmModals from '@/common/dataFeature/trash/TrashConfirmModals.tsx'
+import BoatTable from '@/page/boat/component/BoatTable.tsx'
 
 const BoatTrashPage = () => {
     const {
@@ -76,106 +64,26 @@ const BoatTrashPage = () => {
                         clear: __actionClear,
                     }}
                 />
-                <div className="row overflow-y position-relative">
-                    <div className="col-md-12 table-responsive-md">
-                        <TableThemeLogic
-                            isLoading={__isLoading}
-                            isNoWrap
-                            ths={[
-                                'Name',
-                                'Boat Type',
-                                'Total Photos',
-                                'Total Promo Photos',
-                                'Status Active',
-                                'Created At',
-                                '',
-                            ]}
-                            tds={__list}>
-                            {__list.map((vm, index) => {
-                                return (
-                                    <tr key={index} title="Preview Detail">
-                                        <td>
-                                            <TblLineFirstPrimary
-                                                value={vm?.name}
-                                            />
-                                        </td>
-                                        <td>
-                                            <TblLineFirst
-                                                value={
-                                                    vm?.boatComponentTypeName
-                                                }
-                                            />
-                                        </td>
-                                        <td>
-                                            <TblLineSecond>
-                                                {vm?.photos.length || '-'}
-                                            </TblLineSecond>
-                                        </td>
-                                        <td>
-                                            <TblLineSecond>
-                                                {vm?.promoPhotos.length || '-'}
-                                            </TblLineSecond>
-                                        </td>
-
-                                        <td>
-                                            <TextTrueOrFalse
-                                                value={vm.isActive}
-                                            />
-                                        </td>
-                                        <td>
-                                            <TblLineSecond>
-                                                {vm?.createdAt || '-'}
-                                            </TblLineSecond>
-                                        </td>
-                                        <td>
-                                            <div className="hstack gap-2 justify-content-end">
-                                                <BtnCircleX
-                                                    actions={{
-                                                        click: () => {
-                                                            __handleChoosePermanentRemove(
-                                                                vm,
-                                                            )
-                                                        },
-                                                    }}
-                                                />
-
-                                                <BtnCircleRestore
-                                                    actions={{
-                                                        click: () => {
-                                                            __handleChooseRestore(
-                                                                vm,
-                                                            )
-                                                        },
-                                                    }}
-                                                />
-                                            </div>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                        </TableThemeLogic>
-                    </div>
-                </div>
-
-                {isShowPagination(__isLoading, __list, __pagination) ? (
-                    <Pagination
-                        onMove={(step) => __actionPagination(step)}
-                        className="mt-2"
-                        pagination={configDefaultPagination(
-                            __pagination,
-                            'totalPage',
-                        )}
-                    />
-                ) : null}
+                <BoatTable
+                    isTrash={true}
+                    __list={__list}
+                    __isLoading={__isLoading}
+                    __pagination={__pagination}
+                    actions={{
+                        __actionPagination: __actionPagination,
+                        __handleChoosePermanentRemove: __handleChoosePermanentRemove,
+                        __handleChooseRestore: __handleChooseRestore
+                    }}
+                />
             </CardListData>
 
             <CreatePortalLayout>
                 <TrashConfirmModals
-                    name={__dataRestore?.name  || __dataPermanentRemove?.name}
+                    name={__dataRestore?.name || __dataPermanentRemove?.name}
                     isLoading={__isLoadingTrash}
                     actions={{
                         handleRestore: __handleRestore,
-                        handlePermanentRemove: __handlePermanentRemove
+                        handlePermanentRemove: __handlePermanentRemove,
                     }}
                 />
             </CreatePortalLayout>
