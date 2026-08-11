ebbf0629ca250cda3ed743087eee46086a3d8fc8
ab-azmi <azmi.kiva@gmail.com>
2026-08-11 13:27:50 +0800

    feat: create blog preview page

--- src/page/contentBlog/hook/useContentBlogDetail.hook.ts
+++ src/page/contentBlog/hook/useContentBlogDetail.hook.ts
@@ -1,12 +1,19 @@
-import { useParams } from 'react-router'
+import { useNavigate, useParams } from 'react-router'
 import useDataDetailHook from '@/hook/base/useDataDetail.hook.ts'
 import useLocationStateHook from '@/hook/useLocationState.hook.ts'
 import usePageFlowHandlerHook from '@/hook/usePageFlowHandler.hook.ts'
 import contentBlogPath from '@/path/contentBlog.path.ts'
 import { apiBlogContent } from '@/service/api/contentManage.api.ts'
+import { objDataSearchOther } from '@/config/objectPassState.config.ts'
+import {
+    RESTORE_COUNT,
+    RESTORE_COUNT_ADVANCE,
+    RESTORE_IS_USE_SEARCH,
+} from '@/config/advanceSearch.config.ts'
 
 const useContentBlogDetail = () => {
     const { id } = useParams()
+    const navigate = useNavigate()
 
     const restored = useLocationStateHook()
 
@@ -16,12 +23,21 @@ const useContentBlogDetail = () => {
         triggerBy: id,
     })
 
-    const { __handleToAdd, __handleToEdit, __handleToMain } =
+    const { __handleToAdd, __handleToEdit, __handleToMain, __handleToDetail } =
         usePageFlowHandlerHook({
             basePath: contentBlogPath,
             pathFromKey: 'blog-detail',
             search: restored.dataSearch,
         })
+
+    const _handleToPreview = (id: number|string) => {
+        navigate(contentBlogPath.preview(id), {
+            state: {
+                ...objDataSearchOther(restored.dataSearch),
+                from: contentBlogPath.detail(id),
+            },
+        })
+    }
 
     return {
         __id: id,
@@ -33,6 +49,8 @@ const useContentBlogDetail = () => {
         __handleToAdd,
         __handleToEdit,
         __handleToMain,
+        __handleToPreview: _handleToPreview,
+        __handleToDetail,
     }
 }
 
--- src/path/contentBlog.path.ts
+++ src/path/contentBlog.path.ts
@@ -3,6 +3,7 @@ import { objectPathMenu, pathBasenameRoute } from '@/config/base/objectPath.conf
 const contentBlogPath = {
     ...objectPathMenu('cm-blog'),
     trash: pathBasenameRoute('cm-blog') + '/trash',
+    preview: (id?: string|number) => pathBasenameRoute('cm-blog') + '/' + id + '/detail/preview',
 }
 
 export default contentBlogPath
