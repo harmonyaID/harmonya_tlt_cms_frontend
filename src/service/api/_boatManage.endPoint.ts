570e5e3464d29ddca5f9b9d5ea062824e9660107
ab-azmi <azmi.kiva@gmail.com>
2026-08-12 23:10:30 +0800

    feat: implement trash & restore in boat type

--- src/service/api/_boatManage.endPoint.ts
+++ src/service/api/_boatManage.endPoint.ts
@@ -6,19 +6,23 @@ const baseAPI: any = String(import.meta.env.VITE_BASE_API)
 // Boat
 const baseAPIBoat = baseAPI + '/boats'
 export const SrvBoatCRUD = { ...objectPathEndPointAPI(baseAPIBoat)}
-
 export const SrvBoatTrash = baseAPIBoat + '/trash'
-
 export const SrvBoatTrashWithId = (id: string | number = ''): string =>
     baseAPIBoat + '/trash/' + id
-
 export const SrvBoatRestore = (id: string | number = ''): string =>
     baseAPIBoat + '/trash/' + id + '/restore'
 
+
 // Boat Type
-export const SrvBoatTypeCRUD = objectPathEndPointAPI(
-    SrvBoatCRUD.main + '/components/types',
-)
+const baseAPIBoatType = baseAPIBoat + '/components/types'
+export const SrvBoatTypeCRUD = objectPathEndPointAPI(baseAPIBoatType)
+export const SrvBoatTypeTrash = baseAPIBoatType + '/trash'
+export const SrvBoatTypeTrashWithId = (id: string | number = ''): string =>
+    baseAPIBoatType + '/trash/' + id
+export const SrvBoatTypeRestore = (id: string | number = ''): string =>
+    baseAPIBoatType + '/trash/' + id + '/restore'
+
+
 
 // Boat Contact Forms
 export const SrvBoatContactFormCRUD = objectPathEndPointAPI(
