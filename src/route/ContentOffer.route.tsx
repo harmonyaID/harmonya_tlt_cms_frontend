eb07812ef8687416639475c0cd3cd9a2c5c74a82
ab-azmi <azmi.kiva@gmail.com>
2026-08-27 22:03:39 +0800

    feat: create offer trash page

--- src/route/ContentOffer.route.tsx
+++ src/route/ContentOffer.route.tsx
@@ -2,9 +2,13 @@ import { Route } from 'react-router'
 import contentOfferPath from '@/path/contentOffer.path.ts'
 import { lazy } from 'react'
 import SuspenseLayout from '@/component/layout/Suspense.layout.tsx'
+import Page404Layout from '@/component/layout/Page404.layout.tsx'
 
 const ContentOfferPage = lazy(
     () => import('@/page/contentOffer/ContentOffer.page.tsx'),
+)
+const ContentOfferTrashPage = lazy(
+    () => import('@/page/contentOffer/ContentOfferTrash.page.tsx'),
 )
 
 const ContentOfferRoute = () => {
@@ -19,6 +23,22 @@ const ContentOfferRoute = () => {
                         <ContentOfferPage />
                     </SuspenseLayout>
                 }
+            />
+
+            <Route
+                path={contentOfferPath.trash}
+                element={
+                    <SuspenseLayout
+                        titleNavbar="Offer"
+                        isCheckPermission={false}>
+                        <ContentOfferTrashPage />
+                    </SuspenseLayout>
+                }
+            />
+
+            <Route
+                path="*"
+                element={<Page404Layout to={contentOfferPath.main} />}
             />
         </Route>
     )
