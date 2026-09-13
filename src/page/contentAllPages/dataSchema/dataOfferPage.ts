2af879f32044b7725cf5c8e1452fcaad7598ce99
arbimauday <arbimauday@gmail.com>
2026-09-13 12:22:05 +0800

    feat: offer setting

---
+++ src/page/contentAllPages/dataSchema/dataOfferPage.ts
@@ -0,0 +1,15 @@
+const dataOfferPage = {
+    SECTION1: {
+        backgroundImage: '',
+        title: '',
+    },
+    SECTION2: {
+        title: '',
+    },
+    SECTION3: {
+        content: '',
+        description: '',
+    },
+}
+
+export default dataOfferPage
