ec8c62bf2905d1ffa90221a818f851a3006d6dd0
ab-azmi <azmi.kiva@gmail.com>
2026-09-15 00:19:43 +0800

    feat: Add select option inquiry contact to contact form form

---
+++ src/store/useContactFormInquryType.store.ts
@@ -0,0 +1,27 @@
+import {
+    apiContactFormInquiryType,
+    apiContactFormType,
+    apiLanguage,
+} from '@/service/api/contentManageSetting.api.ts'
+import { createStoreWithAPI } from '@/store/_coreStore/_create.store.ts'
+import { DefaultConfigCreatStoreType } from '@/store/_coreStore/_store.type.ts'
+import useHookFetchDataStore from '@/store/_coreStore/_useHookFetchData.store.ts'
+
+const configUseStore = createStoreWithAPI(() =>
+    apiContactFormInquiryType.list({ page: 0 }),
+)
+
+const useContactFormInquiryTypeStore = (
+    passConfig: DefaultConfigCreatStoreType = {},
+) => {
+    return {
+        ...useHookFetchDataStore({
+            ...passConfig,
+            configUseStore: configUseStore,
+            isFormatList: true,
+            // formatBy: ['code', 'country'],
+        }),
+    }
+}
+
+export default useContactFormInquiryTypeStore
